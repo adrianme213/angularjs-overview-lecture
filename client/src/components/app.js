3f9a8fceee6049f06123d014b515b30ec3bfcd3b
Adrian Meza <adrianme213@gmail.com>
2018-03-24 15:26:44 -0700

    [feat] - angular loads correctly - app has notes added to start

---
+++ client/src/components/app.js
@@ -0,0 +1,9 @@
+angular.module('nba-game-schedule')
+  .component('app', {
+    controller: function() {
+      // CONSOLE LOG SOMETHING TO ENSURE CONTROLLER IS RUNNING
+
+      // LOAD THE TEMPLATE URL (PREFERRED TO INLINE TEMPLATE)
+    },
+    template: `<div>Hello From App</div>`
+  });
